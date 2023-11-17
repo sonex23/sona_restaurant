8e2d43d9576a002c0d9241a5ab9fed6a154740e9
sonex23 <sonaccx7@gmail.com>
2023-11-17 20:57:39 +0700

    config router

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sona_restaurant/core/service/service_locator.dart';
+import 'package:sona_restaurant/misc/router/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,8 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      routerConfig: app<MainRouter>().router,
+      title: 'Sona Restaurant',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
